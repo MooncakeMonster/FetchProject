f23f40f435927ab9338d668cd2fcd24bfd97fbba
jun159 <baojun159@gmail.com>
2015-05-28 19:12:09 +0800

    Update calendar colour

--- app/src/main/java/mooncakemonster/orbitalcalendar/calendar/CalendarFragment.java
+++ app/src/main/java/mooncakemonster/orbitalcalendar/calendar/CalendarFragment.java
@@ -2,15 +2,11 @@ package mooncakemonster.orbitalcalendar.calendar;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
@@ -21,20 +17,12 @@ import java.util.Calendar;
 import java.util.Date;
 
 import mooncakemonster.orbitalcalendar.R;
-import mooncakemonster.orbitalcalendar.event.EventActivity;
 
 public class CalendarFragment extends Fragment {
 
     // caldroid Calendar
     private CaldroidFragment caldroidFragment;
     private FragmentActivity myContext;
-
-    // upcoming Events List
-    private ListView listView;
-    private ArrayAdapter<String> listAdapter;
-    private String[] upcomingEvents = {"Mooncake", "Monster", "Hello", "Bye", "Mooncake",
-            "Monster", "Hello", "Bye", "Mooncake", "Monster", "Hello", "Bye", "Mooncake", "Monster",
-            "Hello", "Bye", "Mooncake", "Monster", "Hello", "Bye"};             // Testing if scrolling works
 
     public CalendarFragment(){}
 
@@ -93,17 +81,6 @@ public class CalendarFragment extends Fragment {
         caldroidFragment.setCaldroidListener(listener);
 
         //if (savedInstanceState == null) { displayView(0); }
-
-        // List of upcoming events
-        listView = (ListView) rootView.findViewById(R.id.listView);
-        listAdapter = new CustomListAdapter(getActivity().getApplicationContext(), R.layout.custom_list, upcomingEvents);
-        listView.setAdapter(listAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(getActivity().getApplicationContext(), EventActivity.class));
-            }
-        });
 
         return rootView;
     }

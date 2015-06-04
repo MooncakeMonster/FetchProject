b6573ebec1cdd609ae5fb92adcf42a25cd759a59
MunKeat <a0129561@u.nus.edu>
2015-06-04 20:37:13 +0800

    Added AppointmentController variable in EventFragment.java

--- app/src/main/java/mooncakemonster/orbitalcalendar/event/EventFragment.java
+++ app/src/main/java/mooncakemonster/orbitalcalendar/event/EventFragment.java
@@ -6,19 +6,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import mooncakemonster.orbitalcalendar.R;
+import mooncakemonster.orbitalcalendar.database.Appointment;
+import mooncakemonster.orbitalcalendar.database.AppointmentController;
 
 /**
  * Created by BAOJUN on 4/6/15.
  */
 public class EventFragment extends Fragment{
 
+    //Set database to allow user to retrieve data to populate EventFragment.java
+    private AppointmentController appointmentDatabase;
+    //List to get all the appointments
+    private List<Appointment> allAppointment;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        //Get all the appointment
+        appointmentDatabase = new AppointmentController(getActivity());
+        appointmentDatabase.open();
+        allAppointment = appointmentDatabase.getAllAppointment();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_fetch_help, container, false);
-
         return rootView;
+    }
+
+    @Override
+    public void onPause()
+    {
+        if(appointmentDatabase != null)
+        {
+            appointmentDatabase.close();
+            appointmentDatabase = null;
+        }
     }
 }

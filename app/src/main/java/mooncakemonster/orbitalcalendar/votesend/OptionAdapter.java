a6d536dc983443d5fa84ca541edfd5a29c8a3520
MunKeat <a0129561@u.nus.edu>
2015-06-28 23:03:56 +0800

    DatePicker and TimePicker in OptionAdapter now reflect first proposed date's appointment date and time
    This is achieved by re-ordering the setting of date and time picker, with setting of values.

--- app/src/main/java/mooncakemonster/orbitalcalendar/votesend/OptionAdapter.java
+++ app/src/main/java/mooncakemonster/orbitalcalendar/votesend/OptionAdapter.java
@@ -52,16 +52,17 @@ public class OptionAdapter extends ArrayAdapter<OptionItem> {
         holder.vote_end_time = (Button) row.findViewById(R.id.option_end_time);
         holder.remove_option = (Button) row.findViewById(R.id.remove_option);
 
-        holder.vote_start_date.setText(optionItem.getEvent_start_date());
-        holder.vote_end_date.setText(optionItem.getEvent_end_date());
-        holder.vote_start_time.setText(optionItem.getEvent_start_time());
-        holder.vote_end_time.setText(optionItem.getEvent_end_time());
-
-        // Date and time picker
+        // Create Date and time picker
         Constant.setButtonDatePicker(getContext(), holder.vote_start_date, 0, "");
         Constant.setButtonDatePicker(getContext(), holder.vote_end_date, 0, "");
         Constant.setButtonTimePicker(getContext(), holder.vote_start_time, 0, "    ");
         Constant.setButtonTimePicker(getContext(), holder.vote_end_time, 0,   "    ");
+
+        //Reset date and time picker to better reflect default value (i.e. first proposed appointment)
+        holder.vote_start_date.setText(optionItem.getEvent_start_date());
+        holder.vote_end_date.setText(optionItem.getEvent_end_date());
+        holder.vote_start_time.setText(optionItem.getEvent_start_time());
+        holder.vote_end_time.setText(optionItem.getEvent_end_time());
 
         // Remove option from list
         holder.remove_option.setOnClickListener(new View.OnClickListener() {

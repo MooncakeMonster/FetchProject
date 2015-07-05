bfb608d3bfb61b487b451269d5d334e15a39c1ff
MunKeat <a0129561@u.nus.edu>
2015-07-06 01:30:50 +0800

    Draft implementation of ImportICSParser.java

--- app/src/main/java/mooncakemonster/orbitalcalendar/ImportExternal/ImportICSParser.java
+++ app/src/main/java/mooncakemonster/orbitalcalendar/ImportExternal/ImportICSParser.java
@@ -3,11 +3,13 @@ package mooncakemonster.orbitalcalendar.ImportExternal;
 import android.app.Activity;
 import android.os.Bundle;
 
-import net.fortuna.ical4j.data.CalendarParserImpl;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import mooncakemonster.orbitalcalendar.database.Appointment;
 
 /**
  * Read all .ics extension
@@ -18,7 +20,6 @@ public class ImportICSParser extends Activity{
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        CalendarParserImpl parse;
     }
 
     public static void icsReader(String file)
@@ -30,6 +31,13 @@ public class ImportICSParser extends Activity{
         long startMillisec = 0;
         long endMillisec = 0;
         String notes = null;
+
+        //For storing appointments in
+        List<Appointment> timetable = new ArrayList<Appointment>();
+        Appointment tempAppt;
+
+        //AppointmentController variable to control the SQLite database
+        //AppointmentController appointmentDatabase = new AppointmentController(this.getApplicationContext());
 
         try
         {
@@ -45,16 +53,108 @@ public class ImportICSParser extends Activity{
             {
                 line = bufferedDataFile.readLine();
 
-                //Split string
-                String[] split = line.split(Pattern.quote(":") , 2);
+                String field;
+                String intermediate;
+                String value;
 
-                String field = split[0];
-                String value = split[1];
+                //Formatting for DTSTART may be different, hence to accommodate for the difference
+                if(line.subSequence(0,8).equals("DTSTART;") || line.subSequence(0,8).equals("DTEND;"))
+                {
+                    String[] split = line.split(("(;)|(:)"), 3);
 
+                    field = split[0];
+                    intermediate = split[1];
+                    value = split[2];
+                }
+                //Otherwise, split string as per normal
+                else
+                {
+                    String[] split = line.split(Pattern.quote(":"), 2);
+
+                    field = split[0];
+                    //Clear value of intermediate
+                    intermediate = null;
+                    value = split[1];
+                }
+
+                //Decide on what to do, based on the field
+                switch(field)
+                {
+                    //Start of a new appointment
+                    case "BEGIN":
+                        if(value.equals("VEVENT"))
+                        {
+                            tempAppt = new Appointment();
+                        }
+                        break;
+
+                    //Get Event Name
+                    case "DESCRIPTION":case "SUMMARY":
+                        event = value;
+                    break;
+
+                    //Get location
+                    case "LOCATION":
+                        location = value;
+
+                    //Get starting time
+                    // TODO: Find different types of formatting available
+                    case "DTSTART":
+                        if(intermediate.equals(""))
+                        {
+                            //Use a SimpleDateFormatter on value
+
+                            //modify to startMillisec
+                        }
+
+                        else if(intermediate.equals(""))
+                        {
+                            //Use a different SimpleDateFormatter on value
+
+                            //modify to startMillisec
+                        }
+                        break;
+
+                    //Get ending time if any
+                    case "DTEND":
+                        //TODO:
+                        break;
+
+                    //Is there a repeat appointment in the future?
+                    case "RRULE":
+                        //TODO: Make for repeat
+                        processRule(value);
+                        break;
+
+                    //TODO: Is there an reminder set for the appointment?
+
+                    case "END":
+                        if(value.equals("VEVENT"))
+                        {
+                            //Insert into database, with what is available
+
+                            //Nuke tempAppt
+                            tempAppt = null;
+                            //Nuke all values
+                            event = null;
+                            location = null;
+                            startMillisec = 0;
+                            endMillisec = 0;
+                            notes = null;
+                        }
+                        break;
+
+                    default:
+                        break;
+
+                }
             }
+
             //Close resources
+            //appointmentDatabase.close();
             bufferedDataFile.close();
             dataFile.close();
+
         }
         catch(Exception e)
         {

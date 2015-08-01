4f4a9f2385d809c1b3f5a5f713036bfed0518e90
MunKeat <a0129561@u.nus.edu>
2015-08-02 05:44:30 +0800

    Debug: Prevent existing OptionItem from regressing to default value when new OptionItem are added in VotingActivity.java
    1. addTextChangedListener was implemented such that any changes made to Button (a subclass of TextView) will register any changes made to the text within the button (or to TextView). Changes then will be reflected in the underlying data structure, OptionItem.
    2. Constant.setButtonDatePicker and Constant.setButtonTimePicker in OptionAdapter.java, used in instantiating each item in OptionAdapter will now take into account of existing values within OptionItem, instead of using the default value 0, which originally result in OptionItem regressing to the default value.

--- app/src/main/java/mooncakemonster/orbitalcalendar/votesend/OptionAdapter.java
+++ app/src/main/java/mooncakemonster/orbitalcalendar/votesend/OptionAdapter.java
@@ -1,13 +1,14 @@
 package mooncakemonster.orbitalcalendar.votesend;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
 
 import mooncakemonster.orbitalcalendar.R;
@@ -35,8 +36,7 @@ public class OptionAdapter extends ArrayAdapter<OptionItem> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
+    public View getView(int position, View row, ViewGroup parent) {
         final Holder holder;
 
         final OptionItem optionItem = objects.get(position);
@@ -53,10 +53,13 @@ public class OptionAdapter extends ArrayAdapter<OptionItem> {
             holder.remove_option = (Button) row.findViewById(R.id.remove_option);
 
             // Create Date and time picker
-            Constant.setButtonDatePicker(getContext(), holder.vote_start_date, Constant.stringToMillisecond(holder.vote_start_date.toString(), new SimpleDateFormat("dd/MM/yyyy")), "");
-            Constant.setButtonDatePicker(getContext(), holder.vote_end_date, 0, "");
-            Constant.setButtonTimePicker(getContext(), holder.vote_start_time, 0, "");
-            Constant.setButtonTimePicker(getContext(), holder.vote_end_time, 0, "");
+            long startDateTimeMil = Constant.stringToMillisecond(optionItem.getEvent_start_date(), optionItem.getEvent_start_time(), Constant.DATEFORMATTER, Constant.TIMEFORMATTER);
+            long endDateTimeMil = Constant.stringToMillisecond(optionItem.getEvent_end_date(),optionItem.getEvent_end_time(),Constant.DATEFORMATTER, Constant.TIMEFORMATTER);
+
+            Constant.setButtonDatePicker(getContext(), holder.vote_start_date, startDateTimeMil, "");
+            Constant.setButtonDatePicker(getContext(), holder.vote_end_date, endDateTimeMil, "");
+            Constant.setButtonTimePicker(getContext(), holder.vote_start_time, startDateTimeMil , "");
+            Constant.setButtonTimePicker(getContext(), holder.vote_end_time, endDateTimeMil, "");
 
             // Remove option from list
             holder.remove_option.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +67,63 @@ public class OptionAdapter extends ArrayAdapter<OptionItem> {
                 public void onClick(View v) {
                     remove(optionItem);
                     notifyDataSetChanged();
+                }
+            });
+
+            //Detect changes made to the text within the button, and store these changes accordingly in the underlying data structure
+            holder.vote_start_date.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    optionItem.setEvent_start_date(s.toString());
+                }
+            });
+
+            holder.vote_end_date.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    optionItem.setEvent_end_date(s.toString());
+                }
+            });
+
+            holder.vote_start_time.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    optionItem.setEvent_start_time(s.toString());
+                }
+            });
+
+            holder.vote_end_time.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    optionItem.setEvent_end_time(s.toString());
                 }
             });
 
